2edc3a8c17c234063980f501f1d0477672e648a0
AfetAgayeva <afetaghayeva.2003@gmail.com>
2021-11-27 00:19:43 +0400

    Add project files.

---
+++ EconomicProgramming/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace EconomicProgramming
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.GC_Result2 = new DevExpress.XtraGrid.GridControl();
+            this.GV_Result2 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.btnCalculate = new DevExpress.XtraEditors.SimpleButton();
+            this.GC_Result = new DevExpress.XtraGrid.GridControl();
+            this.GV_Result = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GC_Result2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GV_Result2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GC_Result)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GV_Result)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.GC_Result2);
+            this.layoutControl1.Controls.Add(this.btnCalculate);
+            this.layoutControl1.Controls.Add(this.GC_Result);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.layoutControlGroup1;
+            this.layoutControl1.Size = new System.Drawing.Size(832, 453);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // GC_Result2
+            // 
+            this.GC_Result2.Location = new System.Drawing.Point(431, 12);
+            this.GC_Result2.MainView = this.GV_Result2;
+            this.GC_Result2.Name = "GC_Result2";
+            this.GC_Result2.Size = new System.Drawing.Size(389, 429);
+            this.GC_Result2.TabIndex = 3;
+            this.GC_Result2.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.GV_Result2});
+            this.GC_Result2.Visible = false;
+            // 
+            // GV_Result2
+            // 
+            this.GV_Result2.GridControl = this.GC_Result2;
+            this.GV_Result2.Name = "GV_Result2";
+            this.GV_Result2.OptionsBehavior.AllowAddRows = DevExpress.Utils.DefaultBoolean.True;
+            this.GV_Result2.OptionsBehavior.AllowDeleteRows = DevExpress.Utils.DefaultBoolean.True;
+            this.GV_Result2.OptionsBehavior.Editable = false;
+            this.GV_Result2.OptionsView.ShowGroupPanel = false;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 419);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(415, 22);
+            this.btnCalculate.StyleController = this.layoutControl1;
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Hesabla";
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // GC_Result
+            // 
+            this.GC_Result.Location = new System.Drawing.Point(12, 12);
+            this.GC_Result.MainView = this.GV_Result;
+            this.GC_Result.Name = "GC_Result";
+            this.GC_Result.Size = new System.Drawing.Size(415, 403);
+            this.GC_Result.TabIndex = 0;
+            this.GC_Result.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.GV_Result});
+            // 
+            // GV_Result
+            // 
+            this.GV_Result.GridControl = this.GC_Result;
+            this.GV_Result.Name = "GV_Result";
+            this.GV_Result.OptionsBehavior.AllowAddRows = DevExpress.Utils.DefaultBoolean.True;
+            this.GV_Result.OptionsBehavior.AllowDeleteRows = DevExpress.Utils.DefaultBoolean.True;
+            this.GV_Result.OptionsView.ShowGroupPanel = false;
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem3,
+            this.layoutControlItem1,
+            this.layoutControlItem2});
+            this.layoutControlGroup1.Name = "Root";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(832, 453);
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.GC_Result;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(419, 407);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.btnCalculate;
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 407);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(419, 26);
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem2.TextVisible = false;
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.GC_Result2;
+            this.layoutControlItem3.Location = new System.Drawing.Point(419, 0);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(393, 433);
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem3.TextVisible = false;
+            this.layoutControlItem3.Visibility = DevExpress.XtraLayout.Utils.LayoutVisibility.Never;
+            // 
+            // Form1
+            // 
+            this.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.Appearance.Options.UseForeColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 453);
+            this.Controls.Add(this.layoutControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.GC_Result2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GV_Result2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GC_Result)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GV_Result)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraGrid.GridControl GC_Result;
+        private DevExpress.XtraGrid.Views.Grid.GridView GV_Result;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraEditors.SimpleButton btnCalculate;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private DevExpress.XtraGrid.GridControl GC_Result2;
+        private DevExpress.XtraGrid.Views.Grid.GridView GV_Result2;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+    }
+}
+
---
+++ EconomicProgramming/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Data;
+using System.Web.UI.WebControls;
+
+namespace EconomicProgramming
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            LoadResult1();
+        }
+
+        private void LoadResult1()
+        {
+            GC_Result.DataSource = CreateFirstDatatable();
+            GV_Result.Columns["№"].Width = 30;
+            GV_Result.Columns["Adı"].Width = 130;
+            GC_Result2.Visible = false;
+            GV_Result.Columns["№"].OptionsColumn.AllowEdit = false;
+            GV_Result.Columns["Adı"].OptionsColumn.AllowEdit = false;
+
+        }
+        private void LoadResult2()
+        {
+            GC_Result2.DataSource = CreateSecondDatatable();
+            GV_Result2.Columns["№"].Width = 30;
+            GV_Result2.Columns["Adı"].Width = 130;
+            GC_Result2.Visible = true;
+            GC_Result.Visible = false;
+            btnCalculate.Visible = false;
+            layoutControlItem3.Visibility = DevExpress.XtraLayout.Utils.LayoutVisibility.Always;
+        }
+
+        public DataTable CreateFirstDatatable()
+        {
+            var dataTable = new DataTable();
+            dataTable.Columns.Add("№", typeof(int));
+            dataTable.Columns.Add("Adı", typeof(string));
+            dataTable.Columns.Add("2001", typeof(decimal));
+            dataTable.Columns.Add("2002", typeof(decimal));
+
+            var row1 = dataTable.NewRow();
+            row1["№"] = 1;
+            row1["Adı"] = "Xammal";
+            dataTable.Rows.Add(row1);
+
+            var row2 = dataTable.NewRow();
+            row2["№"] = 2;
+            row2["Adı"] = "Yanacaq";
+            dataTable.Rows.Add(row2);
+
+            var row3 = dataTable.NewRow();
+            row3["№"] = 3;
+            row3["Adı"] = "Əmək haqqı";
+            dataTable.Rows.Add(row3);
+
+            var row4 = dataTable.NewRow();
+            row4["№"] = 4;
+            row4["Adı"] = "Amortizasiya";
+            dataTable.Rows.Add(row4);
+
+            var row5 = dataTable.NewRow();
+            row5["№"] = 5;
+            row5["Adı"] = "Sosial xərclər";
+            dataTable.Rows.Add(row5);
+
+            var row6 = dataTable.NewRow();
+            row6["№"] = 6;
+            row6["Adı"] = "İdarəetmə";
+            dataTable.Rows.Add(row6);
+
+            var row7 = dataTable.NewRow();
+            row7["№"] = 7;
+            row7["Adı"] = "Sex xərci";
+            dataTable.Rows.Add(row7);
+
+            var row8 = dataTable.NewRow();
+            row8["№"] = 8;
+            row8["Adı"] = "Nəqliyyat xərci";
+            dataTable.Rows.Add(row8);
+
+            var row9 = dataTable.NewRow();
+            row9["№"] = 9;
+            row9["Adı"] = "Kommersiya";
+            dataTable.Rows.Add(row9);
+
+            var row10 = dataTable.NewRow();
+            row10["№"] = 10;
+            row10["Adı"] = "Gəlir";
+            dataTable.Rows.Add(row10);
+
+            return dataTable;
+        }
+        public DataTable CreateSecondDatatable()
+        {
+            DataRow[] rows = new DataRow[GV_Result.RowCount];
+            for (int i = 0; i < GV_Result.RowCount; i++)
+            {
+                rows[i] = GV_Result.GetDataRow(i);
+            }
+            var dataTable = new DataTable();
+            dataTable.Columns.Add("№", typeof(int));
+            dataTable.Columns.Add("Adı", typeof(string));
+            dataTable.Columns.Add("2001", typeof(decimal));
+            dataTable.Columns.Add("2002", typeof(decimal));
+            dataTable.Columns.Add("Mütləqi", typeof(decimal));
+            dataTable.Columns.Add("Nisbi", typeof(decimal));
+
+            var row1 = dataTable.NewRow();
+            row1["№"] = 1;
+            row1["Adı"] = "Xammal";
+            row1["2001"] = rows[0].ItemArray[2];
+            row1["2002"] = rows[0].ItemArray[3];
+            row1["Mütləqi"] = Math.Round(Convert.ToDecimal(rows[0].ItemArray[3]) - Convert.ToDecimal(rows[0].ItemArray[2]),2);
+            row1["Nisbi"] = Math.Round(Convert.ToDecimal(rows[0].ItemArray[3]) / Convert.ToDecimal(rows[0].ItemArray[2]), 2);
+            dataTable.Rows.Add(row1);
+
+            var row2 = dataTable.NewRow();
+            row2["№"] = 2;
+            row2["Adı"] = "Yanacaq";
+            row2["2001"] = rows[1].ItemArray[2];
+            row2["2002"] = rows[1].ItemArray[3];
+            row2["Mütləqi"] = Math.Round(Convert.ToDecimal(rows[1].ItemArray[3]) - Convert.ToDecimal(rows[1].ItemArray[2]),2);
+            row2["Nisbi"] = Math.Round(Convert.ToDecimal(rows[1].ItemArray[3]) / Convert.ToDecimal(rows[1].ItemArray[2]), 2);
+            dataTable.Rows.Add(row2);
+
+            var row3 = dataTable.NewRow();
+            row3["№"] = 3;
+            row3["Adı"] = "Əmək haqqı";
+            row3["2001"] = rows[2].ItemArray[2];
+            row3["2002"] = rows[2].ItemArray[3];
+            row3["Mütləqi"] = Math.Round(Convert.ToDecimal(rows[2].ItemArray[3]) - Convert.ToDecimal(rows[2].ItemArray[2]),2);
+            row3["Nisbi"] = Math.Round(Convert.ToDecimal(rows[2].ItemArray[3]) / Convert.ToDecimal(rows[2].ItemArray[2]), 2);
+            dataTable.Rows.Add(row3);
+
+
+            var row4 = dataTable.NewRow();
+            row4["№"] = 4;
+            row4["Adı"] = "Amortizasiya";
+            row4["2001"] = rows[3].ItemArray[2];
+            row4["2002"] = rows[3].ItemArray[3];
+            row4["Mütləqi"] = Math.Round(Convert.ToDecimal(rows[3].ItemArray[3]) - Convert.ToDecimal(rows[3].ItemArray[2]),2);
+            row4["Nisbi"] = Math.Round(Convert.ToDecimal(rows[3].ItemArray[3]) / Convert.ToDecimal(rows[3].ItemArray[2]), 2);
+            dataTable.Rows.Add(row4);
+
+            var row5 = dataTable.NewRow();
+            row5["№"] = 5;
+            row5["Adı"] = "Sosial xərc";
+            row5["2001"] = rows[4].ItemArray[2];
+            row5["2002"] = rows[4].ItemArray[3];
+            row5["Mütləqi"] = Math.Round(Convert.ToDecimal(rows[4].ItemArray[3]) - Convert.ToDecimal(rows[4].ItemArray[2]),2);
+            row5["Nisbi"] = Math.Round(Convert.ToDecimal(rows[4].ItemArray[3]) / Convert.ToDecimal(rows[4].ItemArray[2]), 2);
+            dataTable.Rows.Add(row5);
+
+            var row6 = dataTable.NewRow();
+            row6["№"] = 6;
+            row6["Adı"] = "İdarəetmə";
+            row6["2001"] = rows[5].ItemArray[2];
+            row6["2002"] = rows[5].ItemArray[3];
+            row6["Mütləqi"] = Math.Round(Convert.ToDecimal(rows[5].ItemArray[3]) - Convert.ToDecimal(rows[5].ItemArray[2]),2);
+            row6["Nisbi"] = Math.Round(Convert.ToDecimal(rows[5].ItemArray[3]) / Convert.ToDecimal(rows[5].ItemArray[2]), 2);
+            dataTable.Rows.Add(row6);
+
+            var row7 = dataTable.NewRow();
+            row7["№"] = 7;
+            row7["Adı"] = "Sex xərci";
+            row7["2001"] = rows[6].ItemArray[2];
+            row7["2002"] = rows[6].ItemArray[3];
+            row7["Mütləqi"] = Math.Round(Convert.ToDecimal(rows[6].ItemArray[3]) - Convert.ToDecimal(rows[6].ItemArray[2]),2);
+            row7["Nisbi"] = Math.Round(Convert.ToDecimal(rows[6].ItemArray[3]) / Convert.ToDecimal(rows[6].ItemArray[2]), 2);
+            dataTable.Rows.Add(row7);
+
+            var row8 = dataTable.NewRow();
+            row8["№"] = 8;
+            row8["Adı"] = "Nəqliyyat xərci";
+            row8["2001"] = rows[7].ItemArray[2];
+            row8["2002"] = rows[7].ItemArray[3];
+            row8["Mütləqi"] = Math.Round(Convert.ToDecimal(rows[7].ItemArray[3]) - Convert.ToDecimal(rows[7].ItemArray[2]),2);
+            row8["Nisbi"] = Math.Round(Convert.ToDecimal(rows[7].ItemArray[3]) / Convert.ToDecimal(rows[7].ItemArray[2]), 2);
+            dataTable.Rows.Add(row8);
+
+            var row9 = dataTable.NewRow();
+            row9["№"] = 9;
+            row9["Adı"] = "Kommersiya";
+            row9["2001"] = rows[8].ItemArray[2];
+            row9["2002"] = rows[8].ItemArray[3];
+            row9["Mütləqi"] = Math.Round(Convert.ToDecimal(rows[8].ItemArray[3]) - Convert.ToDecimal(rows[8].ItemArray[2]),2);
+            row9["Nisbi"] = Math.Round(Convert.ToDecimal(rows[8].ItemArray[3]) / Convert.ToDecimal(rows[8].ItemArray[2]), 2);
+            dataTable.Rows.Add(row9);
+
+            var row10 = dataTable.NewRow();
+            row10["№"] = 10;
+            row10["Adı"] = "Gəlir";
+            row10["2001"] = rows[9].ItemArray[2];
+            row10["2002"] = rows[9].ItemArray[3];
+            row10["Mütləqi"] = Math.Round(Convert.ToDecimal(rows[9].ItemArray[3]) - Convert.ToDecimal(rows[9].ItemArray[2]),2);
+            row10["Nisbi"] = Math.Round(Convert.ToDecimal(rows[9].ItemArray[3]) / Convert.ToDecimal(rows[9].ItemArray[2]), 2);
+            dataTable.Rows.Add(row10);
+
+            var row11 = dataTable.NewRow();
+            row11["№"] = 11;
+            row11["Adı"] = "ƏDV";
+            row11["2001"] = 0.18;
+            row11["2002"] = 0.18;
+            row11["Mütləqi"] = 0;
+            row11["Nisbi"] = 0;
+            dataTable.Rows.Add(row11);
+
+            var row12 = dataTable.NewRow();
+            row12["№"] = 12;
+            row12["Adı"] = "Qiymət ƏDV";
+            row12["2001"] = Math.Round((Convert.ToDouble(rows[0].ItemArray[2]) +
+                Convert.ToDouble(rows[1].ItemArray[2]) +
+                Convert.ToDouble(rows[2].ItemArray[2]) +
+                Convert.ToDouble(rows[3].ItemArray[2]) +
+                Convert.ToDouble(rows[4].ItemArray[2]) +
+                Convert.ToDouble(rows[5].ItemArray[2]) +
+                Convert.ToDouble(rows[6].ItemArray[2]) +
+                Convert.ToDouble(rows[7].ItemArray[2]) +
+                Convert.ToDouble(rows[8].ItemArray[2]) +
+                Convert.ToDouble(rows[9].ItemArray[2])) + (Convert.ToDouble(rows[9].ItemArray[2]) * Convert.ToDouble(row11["2001"])), 2);
+            row12["2002"] = Math.Round((Convert.ToDouble(rows[0].ItemArray[3]) +
+                Convert.ToDouble(rows[1].ItemArray[3]) +
+                Convert.ToDouble(rows[2].ItemArray[3]) +
+                Convert.ToDouble(rows[3].ItemArray[3]) +
+                Convert.ToDouble(rows[4].ItemArray[3]) +
+                Convert.ToDouble(rows[5].ItemArray[3]) +
+                Convert.ToDouble(rows[6].ItemArray[3]) +
+                Convert.ToDouble(rows[7].ItemArray[3]) +
+                Convert.ToDouble(rows[8].ItemArray[3]) +
+                Convert.ToDouble(rows[9].ItemArray[3])) + (Convert.ToDouble(rows[9].ItemArray[3]) * Convert.ToDouble(row11["2002"])), 2);
+            row12["Mütləqi"] = Math.Round(Convert.ToDecimal(row12["2002"]) - Convert.ToDecimal(row12["2001"]),2);
+            row12["Nisbi"] = Math.Round(Convert.ToDecimal(row12["2002"]) / Convert.ToDecimal(row12["2001"]), 2);
+            dataTable.Rows.Add(row12);
+
+
+            return dataTable;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            LoadResult2();
+        }
+
+
+    }
+}
